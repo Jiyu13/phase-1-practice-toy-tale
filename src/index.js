e75793eb450bce9e672f84b41bda04684610402b
Ziru Zou <jiyucoding@yahoo.com>
2023-01-15 16:34:49 -0500

    Fetch toys and  iterator each toy, create new container for each toy and add to the DOM

--- src/index.js
+++ src/index.js
@@ -13,3 +13,45 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+
+function renderOneToy(obj) {
+
+  const cardTag = document.createElement("div");
+  cardTag.className = "card"
+
+  const toyNameTag = document.createElement("h2");
+  toyNameTag.innerHTML = obj.name;
+
+  const toyImgTag = document.createElement("img")
+  toyImgTag.src = obj.image;
+  toyImgTag.className = "toy-avatar";
+
+  const toyLikeCount = document.createElement("p");
+  toyLikeCount.innerHTML = `${obj.likes} likes`
+
+  const toyLikeBtn = document.createElement("button")
+  toyLikeBtn.className = "like-btn";
+  toyLikeBtn.innerHTML = "Like ❤️"
+
+  cardTag.appendChild(toyNameTag)
+  cardTag.appendChild(toyImgTag)
+  cardTag.appendChild(toyLikeCount)
+  cardTag.appendChild(toyLikeBtn)
+
+  // Add toy card to DOM
+  const toyCollection = document.querySelector("#toy-collection");
+  toyCollection.appendChild(cardTag)
+}
+
+
+// Fetch Andy's Toys
+function getAllToys() {
+  return fetch("http://localhost:3000/toys")
+  .then(response => response.json())
+  .then(toysData => toysData.forEach(toy => renderOneToy(toy)))
+  // .then(toysData => console.log(toysData))
+}
+
+
+getAllToys()
